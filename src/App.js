1fbdc3a5215527ae43c36174cde9cbcb6b319576
Yevhen Humen <evgeniy.gumen@gmail.com>
2023-06-21 19:45:37 +0300

    fix: load User profile on default

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
-import './App.css';
+import React, {lazy, Suspense, useEffect} from 'react';
+import "./Aapp.css"
 import Navbar from './components/Navbar/Navbar';
 import {Route, Routes} from 'react-router-dom';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
+//import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginContainer from './components/Login/Login';
@@ -11,36 +11,47 @@ import {connect} from 'react-redux';
 import {initializeApp} from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
 
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+const UsersContainer = lazy(() => import('./components/Users/UsersContainer'));
+
 
 const App = (props) => {
-    useEffect(() => {
-        props.initializeApp();
-    }, [props.initialized])
+  useEffect(() => {
+    props.initializeApp();
+  }, [props.initialized])
 
-    if (!props.initialized) {
-        return <Preloader />
-    }
-    return (
-            <div className='app-wrapper'>
-                <HeaderContainer />
-                <Navbar state={props.state} />
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/profile/:userId?' element={<ProfileContainer />} />
-                        <Route path='/dialogs/*' element={<DialogsContainer />} />
-                        <Route path='/users' element={<UsersContainer />} />
-                        <Route path='/login' element={<LoginContainer />} />
-                    </Routes>
-                </div>
-            </div>
-)
+  if (!props.initialized) {
+    return <Preloader/>
+  }
+  return (
+    <div className='app-wrapper'>
+      <HeaderContainer/>
+      <Navbar state={props.state}/>
+      <div className='app-wrapper-content'>
+        <Routes>
+          <Route path='/profile/:id?' element={<ProfileContainer/>}/>
+          <Route path='/dialogs/*' element={
+            <Suspense fallback={<Preloader/>}>
+              <DialogsContainer/>
+            </Suspense>
+          }/>
+          <Route path='/users' element={
+            <Suspense fallback={<Preloader/>}>
+              <UsersContainer/>
+            </Suspense>
+          }/>
+          <Route path='/login' element={<LoginContainer/>}/>
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
-    return {
-        initialized: state.app.initialized,
-    }
+  return {
+    initialized: state.app.initialized,
+  }
 }
 
 
-export default connect(mapStateToProps, { initializeApp })(App);
+export default connect(mapStateToProps, {initializeApp})(App);
--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -1,44 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { compose } from "redux";
+import React, {useEffect, useState} from "react";
+import {connect} from "react-redux";
+import {compose} from "redux";
 import withParams from "../../HOCs/withParams";
-import { getProfile, getStatus, setProfilePage, updateStatus } from "../../redux/profile-reducer";
+import {getProfile, getStatus, setProfilePage, updateStatus} from "../../redux/profile-reducer";
 import Preloader from "../common/Preloader/Preloader";
 import Profile from "./Profile";
 import withLoginRedirect from "../../HOCs/withLoginRedirect";
+import {useParams} from "react-router-dom";
 
 const ProfileContainer = (props) => {
+  let {id} = useParams();
+  const [userId, setUserId] = useState(props.authorizedUserId);
 
-    const [userId, setUserId] = useState(props.params.userId)
+  if (id && (id !== userId)) {
+    setUserId(id);
+  }
+  if (!id && userId !== props.authorizedUserId) {
+    setUserId(props.authorizedUserId);
+  }
 
-    useEffect(() => {
-        if (!userId) {
-            setUserId(props.authorizedUserId)
-        } else {
-            props.getProfile(userId);
-            props.getStatus(userId);
-        }
-    }, [userId])
+  useEffect(() => {
+    props.getProfile(userId);
+    props.getStatus(userId);
+
+  }, [userId])
 
 
-    return (
-        !props.profile ?
-            <Preloader /> : < Profile profile={props.profile} status={props.status} updateStatus={props.updateStatus} />
-    )
+  return (
+    !props.profile ?
+      <Preloader/> : < Profile profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
+  )
 
 }
 
 
 const mapStateToProps = (state) => {
-    return {
-        authorizedUserId: state.auth.id,
-        profile: state.profilePage.profile,
-        status: state.profilePage.status
-    }
+  return {
+    authorizedUserId: state.auth.id,
+    profile: state.profilePage.profile,
+    status: state.profilePage.status
+  }
 }
 
 export default compose(
-    withParams,
-    connect(mapStateToProps, { setProfilePage, getProfile, getStatus, updateStatus }),
-    withLoginRedirect
+  // withParams,
+  connect(mapStateToProps, {setProfilePage, getProfile, getStatus, updateStatus}),
+  withLoginRedirect
 )(ProfileContainer);
